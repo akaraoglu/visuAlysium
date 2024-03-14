19b648cf1c2f9013a2be0cabcada5e33a56ee56d
ak <akaraoglu@gmail.com>
2024-03-14 16:43:54 +0200

    Updated the folder menu and image viewer to a newer version.

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import os
 import sys
 from PyQt6.QtCore import Qt
-from PyQt6.QtWidgets import QApplication, QMainWindow, QTreeView, QVBoxLayout, QHBoxLayout, QWidget, QListWidget, QListWidgetItem, QLabel, QSplitter, QMenu
+from PyQt6.QtWidgets import QApplication, QMainWindow, QTreeView, QVBoxLayout, QHBoxLayout, QWidget, QListWidget, QListWidgetItem, QLabel, QSplitter, QMenu, QMenuBar, QMessageBox
 from PyQt6.QtGui import QPixmap, QIcon, QFileSystemModel , QAction
 from PyQt6.QtCore import QDir, QStandardPaths, QSize
 import os
@@ -10,12 +10,34 @@ class ImageViewer(QMainWindow):
     def __init__(self):
         super().__init__()
 
-        self.setWindowTitle("Image Viewer")
-        self.setGeometry(100, 100, 800, 600)
+        self.setWindowTitle("Experimental Image Viewer")
 
         self.initUI()
 
     def initUI(self):
+        # Create a menu bar
+        menu_bar = QMenuBar(self)
+        self.setMenuBar(menu_bar)
+
+        # Create a "File" menu and add it to the menu bar
+        file_menu = QMenu("&File", self)
+        menu_bar.addMenu(file_menu)
+
+        # Create actions for the "File" menu
+        open_folder_action = QAction("Open Folder", self)
+        open_image_action = QAction("Open Image", self)
+        about_action = QAction("About", self)
+
+        # Add actions to the "File" menu
+        file_menu.addAction(open_folder_action)
+        file_menu.addAction(open_image_action)
+        file_menu.addAction(about_action)
+
+        # Connect actions to methods
+        open_folder_action.triggered.connect(self.open_folder)
+        open_image_action.triggered.connect(self.open_image)
+        about_action.triggered.connect(self.about)
+
         self.central_widget = QWidget()
         self.setCentralWidget(self.central_widget)
         self.layout = QHBoxLayout(self.central_widget)
@@ -28,9 +50,7 @@ class ImageViewer(QMainWindow):
         self.folder_tree_view.setContextMenuPolicy(Qt.ContextMenuPolicy.CustomContextMenu)
         self.folder_tree_view.customContextMenuRequested.connect(self.open_menu)
         self.folder_tree_view.setRootIndex(self.folder_model.index(''))  # Set the root index to root directory
-
-
-        # # Set default path to user's desktop directory
+                # # Set default path to user's desktop directory
         desktop_path = QStandardPaths.standardLocations(QStandardPaths.StandardLocation.DesktopLocation)[0]
         # self.folder_model.setRootPath(desktop_path)
         desktop_index = self.folder_model.index(desktop_path)
@@ -51,11 +71,12 @@ class ImageViewer(QMainWindow):
         splitter.addWidget(self.image_list_widget)
         splitter.setStretchFactor(0, 0)  # Prevent the folder view from stretching
         splitter.setStretchFactor(1, 1)  # Allow the image view to stretch
-        
+
         # Set default sizes for the left and right panels
         splitter.setSizes([400, 600])
 
         self.layout.addWidget(splitter)
+        self.folder_tree_view.setColumnWidth(0, 250)  # Set the width of the "Name" column
 
     def open_menu(self, position):
         menu = QMenu()
@@ -95,11 +116,94 @@ class ImageViewer(QMainWindow):
         # Retrieve the selected image and perform any necessary action
         pass
 
+    def open_folder(self):
+        # Implement this method to open a folder
+        pass
+
+    def open_image(self):
+        # Implement this method to open an image
+        pass
+        
+    def about(self):
+        # Create a QMessageBox
+        msg_box = QMessageBox()
+        msg_box.setWindowTitle("About")
+
+        # Set text and customize appearance
+        msg_box.setText("This is an experimental Python-based image visualizer and editor.\n\n"
+            "Copyright (c) 2024 Your Name\n\n"
+            "This program is free software: you can redistribute it and/or modify "
+            "it under the terms of the GNU General Public License as published by "
+            "the Free Software Foundation, either version 3 of the License, or "
+            "(at your option) any later version.\n\n"
+            "This program is distributed in the hope that it will be useful, "
+            "but WITHOUT ANY WARRANTY; without even the implied warranty of "
+            "MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the "
+            "GNU General Public License for more details.\n\n"
+            "You should have received a copy of the GNU General Public License "
+            "along with this program.  If not, see <a href='http://www.gnu.org/licenses/'>http://www.gnu.org/licenses/</a>."
+        )
+
+        # Customize appearance
+        msg_box.setStyleSheet("QLabel{min-width: 600px;}")
+        
+        # Set fixed size
+        msg_box.setFixedSize(800, 400)
+
+        # Display the message box
+        msg_box.exec()
+
+
+from PyQt6.QtGui import QPalette, QColor
+
 def main():
     app = QApplication(sys.argv)
+
+    # Set the application style to Fusion
+    app.setStyle("Fusion")
+
+    # Create a dark palette
+    palette = QPalette()
+    palette.setColor(QPalette.ColorRole.Window, QColor(53, 53, 53))
+    palette.setColor(QPalette.ColorRole.WindowText, Qt.GlobalColor.white)
+    palette.setColor(QPalette.ColorRole.Base, QColor(25, 25, 25))
+    palette.setColor(QPalette.ColorRole.AlternateBase, QColor(53, 53, 53))
+    palette.setColor(QPalette.ColorRole.ToolTipBase, Qt.GlobalColor.white)
+    palette.setColor(QPalette.ColorRole.ToolTipText, Qt.GlobalColor.white)
+    palette.setColor(QPalette.ColorRole.Text, Qt.GlobalColor.white)
+    palette.setColor(QPalette.ColorRole.Button, QColor(53, 53, 53))
+    palette.setColor(QPalette.ColorRole.ButtonText, Qt.GlobalColor.white)
+    palette.setColor(QPalette.ColorRole.BrightText, Qt.GlobalColor.red)
+    palette.setColor(QPalette.ColorRole.Link, QColor(42, 130, 218))
+    palette.setColor(QPalette.ColorRole.Highlight, QColor(42, 130, 218))
+    palette.setColor(QPalette.ColorRole.HighlightedText, Qt.GlobalColor.black)
+    app.setPalette(palette)
+
+    # Change some style settings
+    app.setStyleSheet("QToolTip { color: #ffffff; background-color: #2a82da; border: 1px solid white; }")
+
     window = ImageViewer()
+    
+    # Get the screen dimensions
+    screen_rect = app.primaryScreen().availableGeometry()
+
+    # Calculate the dimensions for the window (50% of screen dimensions)
+    window_width = screen_rect.width() * 0.5
+    window_height = screen_rect.height() * 0.75
+
+    # Calculate the position for the window to be centered
+    window_x = (screen_rect.width() - window_width) / 2
+    window_y = (screen_rect.height() - window_height) / 2
+
+    # Set the geometry of the window
+    window.setGeometry(int(window_x), int(window_y), int(window_width), int(window_height))
+
     window.show()
     sys.exit(app.exec())
+
+if __name__ == '__main__':
+    main()
+
 
 if __name__ == '__main__':
     main()
